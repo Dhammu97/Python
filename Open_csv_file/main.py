da5dbec0288705e5643d16390fb899b1db7cd617
Dhammu97 <dhammdipmodak596@gmail.com>
2024-04-10 19:36:42 +0530

    How handel CSV file using pandas and other options

---
+++ Open_csv_file/main.py
@@ -0,0 +1,21 @@
+import pandas
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
+
+
+# with open("weather_data.csv")as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temp = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temp.append(int(row[1]))
+#     print(temp)
+
+
